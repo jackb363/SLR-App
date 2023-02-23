5982efaf158e6db9d54f79f55f31093184d3f143
Jack <jackbutler263@gmail.com>
2023-02-23 10:31:40 +0000

    adjusted model params

--- train_model_py/TrainModel.py
+++ train_model_py/TrainModel.py
@@ -1,26 +1,25 @@
 import numpy as np
 import os
 import util
-import pathlib
-from tensorflow import data
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Dense, Masking
 from tensorflow.keras.callbacks import TensorBoard
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.models import model_from_json
 from tensorflow import lite
 
 # Path for exported data, numpy arrays
-DATA_PATH = os.path.join('C:/Users/Jack/Documents/MediaPipe_SmallDataset')
+DATA_PATH = os.path.join('C:/Users/Jack/Documents/WLASL_Refined_Dataset')
 # Actions that we try to detect
 actions = np.array(os.listdir(DATA_PATH))
 # Videos are going to be 30 frames in length
 
 # logs to monitor training
 log_dir = os.path.join('Logs')
+# invoke on cmd line with 'tensorboard --logdir=.' from 'Logs' dir
 tb_callback = TensorBoard(log_dir=log_dir)
 
 
@@ -56,10 +55,12 @@ def label_frame():
 
 
 # model structure
-def build_model():
+def build_model(max_seq_len):
     # LSTM model 6 Layers
     model = Sequential()
-    model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(None, 1662)))
+    # removes zeroed arrays from sequences / allows for var length sequences
+    model.add(Masking(mask_value=0.0, input_shape=(max_seq_len, 1662)))
+    model.add(LSTM(64, return_sequences=True, activation='relu'))
     model.add(LSTM(128, return_sequences=True, activation='relu'))
     model.add(LSTM(64, return_sequences=False, activation='relu'))
     model.add(Dense(64, activation='relu'))
@@ -70,38 +71,26 @@ def build_model():
 
 
 def train_model(model, X, y):
-    # creates dataset with varied sequence length
-    dataset = data.Dataset.from_tensor_slices((X, y))
-    # Define the batch size
-    batch_size = 32
-
-    # Shuffle and batch the dataset
-    dataset.shuffle(buffer_size=len(X))
-    dataset.batch(batch_size)
-    dataset.prefetch(buffer_size=data.experimental.AUTOTUNE)
-
     # Train/test/val split 80/10/10
-    dataset_train = dataset.take(int(len(sequences) * 0.8))
-    dataset_test = dataset.skip(int(len(sequences) * 0.8))
-    dataset_val = dataset_test.take(len(dataset_test) // 2)
-    dataset_test = dataset_test.skip(len(dataset_val))
-
+    X_train, X_rem, y_train, y_rem = train_test_split(X, y, train_size=0.8)
+    X_test, X_val, y_test, y_val = train_test_split(X_rem, y_rem, test_size=0.5)
+    print(X_train.shape,'\n', X_test.shape, '\n', X_val.shape)
     # stops early if val score has not improved in 5 epochs
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
     # save weights if val score is better than previous
-    model_checkpoint = ModelCheckpoint('res/action.h5', save_best_only=True, monitor='val_loss', mode='min')
+    model_checkpoint = ModelCheckpoint('../res/action.h5', save_best_only=True, monitor='val_loss', mode='min')
 
     model.compile(optimizer='Adam', loss='categorical_crossentropy',
                   metrics=['categorical_accuracy'])
 
-    model.fit(dataset_train, epochs=2000, validation_data=dataset_val,
+    model.fit(X_train, y_train, epochs=2000, validation_data=(X_val, y_val),
               callbacks=[early_stopping, model_checkpoint, tb_callback])
 
     # final weights and structure of trained model saved
-    model.save('action.h5')
+    model.save('../res/action.h5')
 
     # evaluates model after training
-    loss, accuracy = model.evaluate(dataset_test, verbose=0)
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
     print("Test Loss:", loss)
     print("Test Accuracy:", accuracy)
 
@@ -109,7 +98,7 @@ def train_model(model, X, y):
 # saves model structure to json
 def save_model_struc(model):
     model_json = model.to_json()
-    with open('res/model.json', 'w') as json_file:
+    with open('../res/model.json', 'w') as json_file:
         json_file.write(model_json)
 
 
@@ -120,19 +109,19 @@ def save_model_tflite(h5_file):
     converter.target_spec.supported_ops = [lite.OpsSet.TFLITE_BUILTINS, lite.OpsSet.SELECT_TF_OPS]
     converter._experimental_lower_tensor_list_ops = False
     tflite_model = converter.convert()
-    open("res/model.tflite", "wb").write(tflite_model)
+    open("../res/model.json", "wb").write(tflite_model)
 
 
 if __name__ == '__main__':
-    # call to create and save model struc
-    lstm_model = build_model()
-    #save_model_struc(lstm_model)
-
     # call to label and group frames of same videos
     sequences, labels = label_frame()
-    X = np.array(np.concatenate(sequences, axis=0))
+    max_len = max(len(seq) for seq in sequences)
+    X = pad_sequences(sequences, maxlen=max_len, padding='post', dtype='float32')
     y = to_categorical(labels).astype(int)
+    # call to create and save model struc
+    lstm_model = build_model(max_len)
+    save_model_struc(lstm_model)
 
     # call train and pass model struc, frame sequences and associated labels
     train_model(lstm_model, X, y)
-    #save_model_tflite('res/action.h5')
+    #save_model_tflite('../res/action.h5')
